5390a750bdc2c4831d0659627e1038fb46df6b73
ayushamleh <ayushamlesh@gmail.com>
2023-01-24 16:33:52 +0530

    creating model, for ceating table in database 1.added menu model into models,py file 2. In settings.py file updated installed_app 3.will be creating migration in next commit

--- chefs_project/chefsProject/settings.py
+++ chefs_project/chefsProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chefsApp.aaps.chefsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
