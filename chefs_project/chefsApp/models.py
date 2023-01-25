85c52c1ff86a7b308fa058d3a402708b5acaceb2
ayushamleh <ayushamlesh@gmail.com>
2023-01-25 10:44:07 +0530

    basic update

--- chefs_project/chefsApp/models.py
+++ chefs_project/chefsApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from unicodedata import name
 # Create your models here.
 
 
--- chefs_project/chefsProject/settings.py
+++ chefs_project/chefsProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'chefsApp.aaps.chefsAppConfig',
+    'chefsApp.apps.ChefsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
